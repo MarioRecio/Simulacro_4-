c925675648c45d7970be13d6616e2b4e541e775f
MarioRecio <119108864+MarioRecio@users.noreply.github.com>
2022-11-26 11:53:46 +0100

    Update DBUtil.java

--- DBUtil.java
+++ DBUtil.java
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package simulacro.pkg4;
-
-import java.sql.*;
-
-/**
- *
- * @author Nacho
- */
 public class DBUtil {
 
     public Connection conn;
